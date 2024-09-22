070a51f89243820b4de7e0ac69cfdf95b5f281ed
David Herrera <david.gmzherrera28@gmail.com>
2024-09-22 17:18:12 -0600

    se agrego pusher

---
+++ resources/views/broadcast.blade.php
@@ -0,0 +1,4 @@
+<div class="right message">
+  <p>{{$message}}</p>
+  <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Profile picture">
+</div>
---
+++ resources/views/receive.blade.php
@@ -0,0 +1,4 @@
+<div class="left message">
+  <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Avatar">
+  <p>{{$message}}</p>
+</div>
