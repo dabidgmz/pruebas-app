ec6b802ffe5f1d9151f7b834b4b22a2790d82004
David Herrera <david.gmzherrera28@gmail.com>
2024-09-22 14:47:53 -0600

    Se agrego crud de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,99 @@ class UserController extends Controller
 {
     public function index()
     {
-        
+        $users = User::all();
+        return response()->json([
+            'message' => 'Usuarios listados exitosamente', 
+            'data' => $users
+        ], 200); 
     }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Usuario no encontrado'
+            ], 404); 
+        }
+
+        return response()->json([
+            'message' => 'Usuario encontrado', 
+            'data' => $user], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6'
+        ], [
+            'name.required' => 'El nombre es obligatorio.',
+            'email.required' => 'El correo electrónico es obligatorio.',
+            'email.email' => 'El formato del correo electrónico no es válido.',
+            'email.unique' => 'Este correo electrónico ya está registrado.',
+            'password.required' => 'La contraseña es obligatoria.',
+            'password.min' => 'La contraseña debe tener al menos 6 caracteres.'
+        ]);
+
+        $validatedData['password'] = Hash::make($request->password);
+
+        $user = User::create($validatedData);
+
+        return response()->json([
+            'message' => 'Usuario creado exitosamente', 
+            'data' => $user
+        ], 201); 
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['
+            message' => 'Usuario no encontrado'
+        ], 404);
+        }
+        $validatedData = $request->validate([
+            'name' => 'string|max:255',
+            'email' => 'email|unique:users,email,'.$user->id,
+            'password' => 'nullable|min:6'
+        ], [
+            'email.email' => 'El formato del correo electrónico no es válido.',
+            'email.unique' => 'Este correo electrónico ya está registrado para otro usuario.',
+            'password.min' => 'La contraseña debe tener al menos 6 caracteres.'
+        ]);
+
+        if ($request->has('password')) {
+            $validatedData['password'] = Hash::make($request->password);
+        }
+
+        $user->update($validatedData);
+
+        return response()->json([
+            'message' => 'Usuario actualizado exitosamente', 
+            'data' => $user
+        ], 200);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Usuario no encontrado'
+            ], 404);
+        }
+
+        $user->delete();
+
+        return response()->json([
+            'message' => 'Usuario eliminado exitosamente'
+        ], 200);
+    }
+
 }
